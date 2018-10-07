aee270396c573d5f9fb503b852da8ae1b0076e33
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-07 09:04:46 -0700

    Update hcl reference version link

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! An implementation of the Hashicorp Configuration Language.
 //!
 //! References:
-//!     - [hashicorp/hcl](https://github.com/hashicorp/hcl)
+//!     - [hashicorp/hcl](https://github.com/hashicorp/hcl/tree/v1.0.0)
 
 #[macro_use]
 extern crate nom;
