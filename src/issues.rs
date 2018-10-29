1691895043f3707aa5282c128fdcbecda5efbf71
Adrien Thebo <adrien@lagrange-automation.io>
2018-10-29 16:26:50 -0700

    Fixup HCL issue numbers

--- src/issues.rs
+++ src/issues.rs
@@ -16,13 +16,19 @@ pub 'static
 // TODO: look at how Henrik implemented issues in Puppet
 
 pub enum ErrorCode {
-    E00001 = 1
+    E00001 = 1,
+    E00002 = 2,
+    E00003 = 3,
+    E00004 = 4
 }
 
 impl std::string::ToString for ErrorCode {
     fn to_string(&self) -> String {
         match self {
-            E00001 => "Expected LBRACE (eg '{')".to_string(),
+            ErrorCode::E00001 => "Expected LBRACE (eg '{')".to_string(),
+            ErrorCode::E00002 => "Expected RBRACE (eg '}')".to_string(),
+            ErrorCode::E00003 => "Expected Newline".to_string(),
+            ErrorCode::E00004 => "Unexpected token".to_string(),
         }
     }
 }
